5fcb8d3601d7755ae14d77811297a47566a18193
Rhio Adjie Fabian <rhioadjie20@gmail.com>
2021-12-07 11:11:45 +0700

    First commit

---
+++ src/main/java/com/xtremax/training/simplespringapplication/dao/NewsRepository.java
@@ -0,0 +1,10 @@
+package com.xtremax.training.simplespringapplication.dao;
+
+import com.xtremax.training.simplespringapplication.model.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+
+}
